e4d080ef67bc4b3c55d772c9e7cb2793c0159d68
675547 <everis.tiago.lisboa.braz@externo.bancobpi.pt>
2019-05-31 16:45:30 +0100

    properties

--- src/main/java/com/lynxmind/demo/api/ConsultantsApi.java
+++ src/main/java/com/lynxmind/demo/api/ConsultantsApi.java
@@ -1,20 +1,22 @@
 package com.lynxmind.demo.api;
 
-import com.lynxmind.demo.integration.Producer;
+import com.lynxmind.demo.business.consultants.Consultant;
+import com.lynxmind.demo.business.consultants.ConsultantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ConsultantsApi {
 
     @Autowired
-    Producer producer;
+    ConsultantService consultantService;
 
     @GetMapping("/hello")
-    public String hello() {
-        producer.sendMessage("top kek");
-        return "hello worldz";
+    public List<Consultant> hello() {
+        return consultantService.listConsultants();
     }
 
 
--- src/main/java/com/lynxmind/demo/business/consultants/Consultant.java
+++ src/main/java/com/lynxmind/demo/business/consultants/Consultant.java
@@ -1,10 +1,25 @@
 package com.lynxmind.demo.business.consultants;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Consultant {
 
+    @Id
+    private String id;
 
+    private String firstName;
+
+    private String lastName;
 
 }
