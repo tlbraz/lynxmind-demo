e993164661fc0c7e4cd2b17d482447d967c59530
675547 <everis.tiago.lisboa.braz@externo.bancobpi.pt>
2019-05-31 12:05:25 +0100

    Initial commit

---
+++ src/main/java/com/lynxmind/demo/api/ConsultantsApi.java
@@ -0,0 +1,14 @@
+package com.lynxmind.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConsultantsApi {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
