7cd51a9a73734fa7b777b9ed067d68e59a5300c5
675547 <everis.tiago.lisboa.braz@externo.bancobpi.pt>
2019-05-31 14:54:08 +0100

    properties

--- src/main/java/com/lynxmind/demo/api/ConsultantsApi.java
+++ src/main/java/com/lynxmind/demo/api/ConsultantsApi.java
@@ -1,14 +1,22 @@
 package com.lynxmind.demo.api;
 
+import com.lynxmind.demo.integration.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConsultantsApi {
 
+    @Autowired
+    Producer producer;
+
     @GetMapping("/hello")
     public String hello() {
+        producer.sendMessage("top kek");
         return "hello worldz";
     }
+
+
 
 }
